3ddf749b14cec52c9bf32db206ab1328b8aaf0bb
tishka25 <teodorstanishev@gmail.com>
2021-12-04 20:38:23 +0200

    add root navigation implementation

---
+++ app/src/rootNavigation.tsx
@@ -0,0 +1,49 @@
+import { createNavigationContainerRef } from '@react-navigation/native';
+import { StackActions } from '@react-navigation/native';
+
+export type RootStackParamList = {
+  Register: any;
+  Login: any;
+};
+
+export type RootNavigationScreen = keyof RootStackParamList;
+
+export const navigationRef = createNavigationContainerRef<RootStackParamList>();
+
+export function navigate(name: RootNavigationScreen, params?: any) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate<RootNavigationScreen>(name, params);
+  }
+}
+
+export function push(name: string, params?: any) {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.push(name, params));
+  }
+}
+
+export function reset(name: RootNavigationScreen) {
+  if (navigationRef.isReady()) {
+    navigationRef.reset({ index: 0, routes: [{ name: name }] });
+  }
+}
+
+export function goBack() {
+  if (navigationRef.isReady()) {
+    navigationRef.goBack();
+  }
+}
+
+export function getCurrentRoute() {
+  if (navigationRef.isReady()) {
+    return navigationRef.getCurrentRoute();
+  }
+}
+
+export default {
+  navigate,
+  push,
+  reset,
+  goBack,
+  getCurrentRoute,
+};
