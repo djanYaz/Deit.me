827e13e0c0e259a73cead5bc10963642120983de
tishka25 <teodorstanishev@gmail.com>
2021-12-05 00:58:33 +0200

    add mockup; rendering of profiles

--- app/src/screens/Main.tsx
+++ app/src/screens/Main.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import ProfileCard from '../components/ProfileCard';
+import ProfileCard, { ProfileCardProps } from '../components/ProfileCard';
 import ScreenView from '../components/ScreenView';
 
+const profiles: ProfileCardProps[] = [
+  {
+    name: 'Rado1',
+  },
+  {
+    name: 'Rado2',
+  },
+  {
+    name: 'Rado3',
+  },
+];
+
 export default function Main() {
-  return (
-    <ScreenView style={styles.container}>
-      <ProfileCard />
-    </ScreenView>
-  );
+  const [profileIndex, setProfileIndex] = useState(0);
+
+  useEffect(() => {
+    console.log('profile index', profileIndex);
+  }, [profileIndex]);
+
+  function handleDiscard() {
+    const index = profileIndex + 1;
+    if (index >= profiles.length) {
+      setProfileIndex(0);
+    } else {
+      setProfileIndex(index);
+    }
+  }
+
+  function renderProfiles() {
+    const profile = profiles[profileIndex];
+    return (
+      <ProfileCard
+        name={profile.name}
+        key={profileIndex}
+        onDiscard={handleDiscard}
+      />
+    );
+  }
+  return <ScreenView style={styles.container}>{renderProfiles()}</ScreenView>;
 }
 
 const styles = StyleSheet.create({
