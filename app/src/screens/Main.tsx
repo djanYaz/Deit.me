6fab49c3a619521cac16948fa8638b7c74d3044e
tishka25 <teodorstanishev@gmail.com>
2021-12-19 23:44:09 +0200

    retrieve profiles from remote server

--- app/src/screens/Main.tsx
+++ app/src/screens/Main.tsx
@@ -2,64 +2,88 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import ProfileCard, { ProfileCardProps } from '../components/ProfileCardView';
 import ScreenView from '../components/ScreenView';
-import { getRandomArbitrary } from '../utils';
-
-const profiles: ProfileCardProps[] = [
-  {
-    name: 'Rado1',
-    hobbies: ['MMA', 'Cross', 'Box', 'Macki', 'BMW'],
-    pictures: [],
-    age: 21,
-    description:
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    location: 'Sofia',
-  },
-  {
-    name: 'Rado2',
-    hobbies: ['BMW', 'Macki', 'Cici', 'Dupe'],
-    pictures: [],
-    age: 21,
-    description:
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    location: 'Sofia',
-  },
-];
-
+import { API_URL } from '../constants';
+import user from '../user';
+import { makeRequest } from '../utils';
+//TODO: Refactor
+export interface ProfileDTO {
+  token: string;
+  user: {
+    id: 6;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    preference: string;
+    description: string;
+    hobby: { id: number; hobby: string }[];
+  };
+}
 export default function Main() {
-  const [profileIndex, setProfileIndex] = useState(0);
-
+  const [profile, setProfile] = useState<ProfileDTO | undefined>();
+  const [loading, setLoading] = useState(false);
   // Fill in random pictures
+  // useEffect(() => {
+  //   profiles.forEach(profile => {
+  //     const picturesLen = getRandomArbitrary(1, 5);
+  //     for (var i = 0; i < picturesLen; i++) {
+  //       profile.pictures.push(
+  //         'https://picsum.photos/200/300?random=' +
+  //           Math.floor(getRandomArbitrary(1, 20)),
+  //       );
+  //     }
+  //   });
+  // }, []);
+
+  async function handleNewProfile() {
+    console.log('getting new profile');
+    setLoading(true);
+    const newProfile = await makeRequest(
+      API_URL + 'api/browse?shouldMatchHobbies=0',
+      'GET',
+      undefined,
+      user.getCredentials(),
+    );
+    if (newProfile) {
+      setProfile({ ...newProfile.data });
+    }
+  }
+  // Load initial profile
   useEffect(() => {
-    profiles.forEach(profile => {
-      const picturesLen = getRandomArbitrary(1, 5);
-      for (var i = 0; i < picturesLen; i++) {
-        profile.pictures.push(
-          'https://picsum.photos/200/300?random=' +
-            Math.floor(getRandomArbitrary(1, 20)),
-        );
-      }
-    });
+    handleNewProfile();
   }, []);
+
   useEffect(() => {
-    console.log('profile index', profileIndex);
-  }, [profileIndex]);
+    // New profile
+    setLoading(false);
+    console.log('profile', profile);
+  }, [profile]);
 
   function handleDiscard() {
-    const index = profileIndex + 1;
-    if (index >= profiles.length) {
-      setProfileIndex(0);
-    } else {
-      setProfileIndex(index);
-    }
+    handleNewProfile();
   }
 
   function renderProfiles() {
-    const profile = profiles[profileIndex];
+    const profileUser = profile?.user;
+    if (profileUser === undefined) {
+      return null;
+    }
+    const _profile: ProfileCardProps = {
+      name: `${profileUser.firstName} ${profileUser?.lastName}`,
+      hobbies: profileUser.hobby.map(h => h.hobby),
+      pictures: [],
+      description: profileUser.description,
+      location: '',
+      age: 0,
+    };
     return (
-      <ProfileCard key={Date.now()} onSwipe={handleDiscard} {...profile} />
+      <ProfileCard key={Date.now()} onSwipe={handleDiscard} {..._profile} />
     );
   }
-  return <ScreenView style={styles.container}>{renderProfiles()}</ScreenView>;
+  return (
+    <ScreenView loading={loading} style={styles.container}>
+      {renderProfiles()}
+    </ScreenView>
+  );
 }
 
 const styles = StyleSheet.create({
