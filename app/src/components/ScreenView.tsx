02692667e2ea37f4225b6c9385acf8410aef3dd9
tishka25 <teodorstanishev@gmail.com>
2021-12-19 23:43:16 +0200

    add loading indicator to screen view

--- app/src/components/ScreenView.tsx
+++ app/src/components/ScreenView.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, ViewProps } from 'react-native';
+import { ActivityIndicator, StyleSheet, View, ViewProps } from 'react-native';
 
-export default function ScreenView(props: ViewProps) {
+export interface ScreenViewProps extends ViewProps {
+  loading?: boolean;
+}
+export default function ScreenView(props: ScreenViewProps) {
   return (
     <View {...props} style={[props.style, styles.container]}>
       {props.children}
+      {props.loading && (
+        <ActivityIndicator style={styles.loading} size="large" />
+      )}
     </View>
   );
 }
@@ -13,5 +19,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'rgba(127, 93, 240, 0.1)',
+  },
+  loading: {
+    position: 'absolute',
+    marginHorizontal: '50%',
+    marginVertical: '50%',
   },
 });
