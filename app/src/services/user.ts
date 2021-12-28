27d7401b5188879a323240c4d576ed9d080e36ef
tishka25 <teodorstanishev@gmail.com>
2021-12-28 23:35:00 +0200

    add persisting storage for jwt token

--- app/src/services/user.ts
+++ app/src/services/user.ts
@@ -1,34 +1,41 @@
 import { API_URL } from '../constants';
 import { UserCredentials, UserRegisterDTO } from '../types';
 import { makeRequest } from '../utils';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class User {
   private userCredentials: UserCredentials | undefined;
-  async login(email: string, password: string) {
-    const response = await makeRequest(API_URL + 'auth/signin', 'POST', {
-      email,
-      password,
-    });
-    if (response?.status === 200) {
+
+  async handleAuth(response: { status: number; data: any } | undefined) {
+    if (response && response.status === 200) {
       this.userCredentials = { ...response.data };
-      return response.data;
+      await AsyncStorage.setItem('user', JSON.stringify(this.userCredentials));
+      return { ...this.userCredentials };
     } else {
       return false;
     }
   }
 
+  async login(email: string, password: string) {
+    const response = await makeRequest(API_URL + 'auth/signin', 'POST', {
+      email,
+      password,
+    });
+    return await this.handleAuth(response);
+  }
+
   async register(info: UserRegisterDTO) {
     try {
       const response = await makeRequest(API_URL + 'auth/signup', 'POST', info);
-      if (response?.status === 200) {
-        // this.userCredentials = { ...json };
-        return response.data;
-      } else {
-        return false;
-      }
+      return await this.handleAuth(response);
     } catch (error) {
       return false;
     }
+  }
+
+  async logout() {
+    this.userCredentials = undefined;
+    await AsyncStorage.removeItem('user');
   }
 
   getCredentials() {
