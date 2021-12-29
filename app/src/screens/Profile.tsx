a62909963b30cfbef20bd24ecb800ba14035b01f
tishka25 <teodorstanishev@gmail.com>
2021-12-29 12:04:37 +0200

    add signout funcionality

--- app/src/screens/Profile.tsx
+++ app/src/screens/Profile.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
+import CustomButton from '../components/CustomButton';
 import ScreenView from '../components/ScreenView';
+import rootNavigation from '../rootNavigation';
+import user from '../services/user';
 
 export default function Profile() {
+  async function handleLogOut() {
+    await user.logout();
+    rootNavigation.reset('Login');
+  }
   return (
     <ScreenView style={styles.container}>
       <Text style={{ color: 'black', fontSize: 32 }}>Profile screen</Text>
+      <CustomButton title="Log out" onPress={handleLogOut} />
     </ScreenView>
   );
 }
